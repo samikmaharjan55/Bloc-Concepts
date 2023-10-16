2c21fc0580ec65e22b2a691b677474d42f8ab90c
Samik Maharjan <samikmaharjan55@gmail.com>
2023-10-16 12:35:53 +0545

    Test Complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_concepts/cubit/counter_cubit.dart';
 
 void main() {
+  final CounterState counterState1 = CounterState(counterValue: 1);
+  final CounterState counterState2 = CounterState(counterValue: 1);
   runApp(const MyApp());
 }
 
